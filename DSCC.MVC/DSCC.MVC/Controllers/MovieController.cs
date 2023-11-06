d6a06ee31ad255c9842b8bf941c5d99999eea369
Anastasiya <00012252wiut@gmail.com>
2023-11-06 23:16:59 +0500

    added comments for controller

--- DSCC.MVC/DSCC.MVC/Controllers/MovieController.cs
+++ DSCC.MVC/DSCC.MVC/Controllers/MovieController.cs
@@ -8,9 +8,9 @@ namespace DSCC.MVC.Controllers
 {
     public class MovieController : Controller
     {
-        private const string BASE_URL = "https://localhost:44361/";
-        private readonly Uri ClientBasAddress = new Uri(BASE_URL);
-        private readonly HttpClient _client;
+        private const string BASE_URL = "https://localhost:44361/";  // The base URL of the API
+        private readonly Uri ClientBasAddress = new Uri(BASE_URL);  // The base address for the API client
+        private readonly HttpClient _client; // HTTP client for making API requests
 
         public MovieController()
         {
@@ -18,7 +18,7 @@ namespace DSCC.MVC.Controllers
             _client.BaseAddress = ClientBasAddress;
         }
 
-        private void HeaderClearing()
+        private void HeaderClearing()   // Clear default HTTP request headers and set the request type to JSON
         {
             // Clearing default headers
             _client.DefaultRequestHeaders.Clear();
@@ -29,12 +29,15 @@ namespace DSCC.MVC.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movies = new List<Movie>();
+            var movies = new List<Movie>(); // Initialize a list of movies
             HeaderClearing();
+
+            // Send an HTTP GET request to retrieve all movies
             HttpResponseMessage httpResponseMessage = await _client.GetAsync("api/Movies");
 
             if (httpResponseMessage.IsSuccessStatusCode)
             {
+                // Read the response content and deserialize it to a list of movies
                 var responseMessage = await httpResponseMessage.Content.ReadAsStringAsync();
                 movies = JsonConvert.DeserializeObject<List<Movie>>(responseMessage);
             }
@@ -43,13 +46,16 @@ namespace DSCC.MVC.Controllers
 
         public async Task<ActionResult> Details(int id)
         {
+            // Initialize a movie object
             Movie movie = new Movie();
             HeaderClearing();
 
+            // Send an HTTP GET request to retrieve a specific movie by ID
             HttpResponseMessage httpResponseMessage = await _client.GetAsync($"api/Movies/{id}");
 
             if (httpResponseMessage.IsSuccessStatusCode)
             {
+                // Read the response content and deserialize it to a movie object
                 string responseMessage = httpResponseMessage.Content.ReadAsStringAsync().Result;
                 movie = JsonConvert.DeserializeObject<Movie>(responseMessage);
             }
@@ -59,7 +65,7 @@ namespace DSCC.MVC.Controllers
         // GET: Movie/Create
         public ActionResult Create()
         {
-            Movie movie = new Movie();
+            Movie movie = new Movie();  // Initialize a new movie object for creating a new movie
             HeaderClearing();
             return View(movie);
         }
@@ -71,8 +77,11 @@ namespace DSCC.MVC.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Serialize the movie object to JSON and create a string content for the request
                 string createGenreInfo = JsonConvert.SerializeObject(movie);
                 StringContent stringContentInfo = new StringContent(createGenreInfo, Encoding.UTF8, "application/json");
+
+                // Send an HTTP POST request to create a new movie.
                 HttpResponseMessage createHttpResponseMessage = _client.PostAsync(_client.BaseAddress + "api/Movies", stringContentInfo).Result;
                 if (createHttpResponseMessage.IsSuccessStatusCode)
                 {
@@ -85,13 +94,15 @@ namespace DSCC.MVC.Controllers
         // GET: Movie/Edit/5
         public async Task<ActionResult> Edit(int id)
         {
-            Movie movie = new Movie();
+            Movie movie = new Movie();  // Initialize a movie object for editing
             HeaderClearing();
 
+            // Send an HTTP GET request to retrieve a specific movie by ID for editing
             HttpResponseMessage httpResponseMessage = await _client.GetAsync($"api/Movies/{id}");
 
             if (httpResponseMessage.IsSuccessStatusCode)
             {
+                // Read the response content and deserialize it to a movie object
                 string responseMessage = httpResponseMessage.Content.ReadAsStringAsync().Result;
                 movie = JsonConvert.DeserializeObject<Movie>(responseMessage);
             }
@@ -106,8 +117,11 @@ namespace DSCC.MVC.Controllers
         {
             if (ModelState.IsValid)
             {
+                // Serialize the movie object to JSON and create a string content for the request
                 string createSubjectInfo = JsonConvert.SerializeObject(movie);
                 StringContent stringContentInfo = new StringContent(createSubjectInfo, Encoding.UTF8, "application/json");
+
+                // Send an HTTP PUT request to update a specific movie by ID
                 HttpResponseMessage editHttpResponseMessage = _client.PutAsync(_client.BaseAddress + $"api/Movies/{id}", stringContentInfo).Result;
                 if (editHttpResponseMessage.IsSuccessStatusCode)
                 {
@@ -120,13 +134,15 @@ namespace DSCC.MVC.Controllers
         // GET: Subject/Delete/5
         public async Task<ActionResult> Delete(int id)
         {
-            Movie movie = new Movie();
+            Movie movie = new Movie();  // Initialize a movie object for deleting
             HeaderClearing();
 
+            // Send an HTTP GET request to retrieve a specific movie by ID for deletion
             HttpResponseMessage httpResponseMessage = await _client.GetAsync($"api/Movies/{id}");
 
             if (httpResponseMessage.IsSuccessStatusCode)
             {
+                // Read the response content and deserialize it to a movie object
                 string responseMessage = httpResponseMessage.Content.ReadAsStringAsync().Result;
                 movie = JsonConvert.DeserializeObject<Movie>(responseMessage);
             }
@@ -138,6 +154,7 @@ namespace DSCC.MVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, Movie subject)
         {
+            // Send an HTTP DELETE request to delete a specific movie by ID
             HttpResponseMessage deleteSubjectHttpResponseMessage = _client.DeleteAsync(_client.BaseAddress + $"api/Movies/{id}").Result;
             if (deleteSubjectHttpResponseMessage.IsSuccessStatusCode)
             {
