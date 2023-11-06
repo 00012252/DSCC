3d154ebfe21d775852a959f6db130acac12c5690
Anastasiya <00012252wiut@gmail.com>
2023-11-06 23:06:55 +0500

    added comments to controller and dbcontext

--- DSCC.API/DSCC.API/Controllers/MoviesController.cs
+++ DSCC.API/DSCC.API/Controllers/MoviesController.cs
@@ -19,18 +19,21 @@ namespace DSCC.API.Controllers
         [HttpGet]
         public ActionResult GetAllMovies()
         {
+            // Return a list of all movies in the database.
             return new OkObjectResult(_context.Movies.ToList());
         }
 
         [HttpGet("{id}")]
         public ActionResult GetMovie(int id)
         {
+            // Find and return a specific movie by its ID.
             return new OkObjectResult(_context.Movies.Find(id));
         }
 
         [HttpPost]
         public ActionResult PostMovie([FromBody] Movie movie)
         {
+            // Add a new movie to the database and save changes.
             _context.Movies.Add(movie);
             _context.SaveChanges();
             return Ok();
@@ -39,11 +42,18 @@ namespace DSCC.API.Controllers
         [HttpPut("{id}")]
         public ActionResult PutMovie([FromBody] Movie movie, int id)
         {
+            // Find the old movie by its ID.
             var oldMovie = _context.Movies.Find(id);
+
+            // Set the state of the old movie to Modified for updating.
             _context.Entry(oldMovie).State = EntityState.Modified;
+
+            // Update the properties of the old movie with the new movie's data.
             oldMovie.Title = movie.Title;
             oldMovie.Description = movie.Description;
             oldMovie.Genre = movie.Genre;
+
+            // Save changes to apply the update.
             _context.SaveChanges();
             return Ok();
         }
@@ -51,7 +61,10 @@ namespace DSCC.API.Controllers
         [HttpDelete("{id}")]
         public ActionResult DeleteMovie(int id)
         {
+            // Find and remove a specific movie by its ID from the database.
             _context.Remove(_context.Movies.Find(id));
+
+            // Save changes to apply the deletion.
             _context.SaveChanges();
             return Ok();
         }
--- DSCC.API/DSCC.API/MovieDbContext/MovieContext.cs
+++ DSCC.API/DSCC.API/MovieDbContext/MovieContext.cs
@@ -4,9 +4,17 @@ using DSCC.API.Models;
 namespace DSCC.API.MovieDbContext
 
 {
+    // Represents the database context for working with Movie entities
     public class MovieContext : DbContext
     {
-        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { Database.EnsureCreated(); }
+        // Constructor that takes DbContextOptions for configuring the context.
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) {
+            // Ensure that the database is created when the context is initialized.
+            // This method creates the database if it doesn't exist.
+            Database.EnsureCreated(); }
+
+        // Represents a DbSet of Movie entities in the database.
+        // This DbSet can be used to query and manipulate Movie data.
         public DbSet<Movie> Movies { get; set; }
     }
 }
