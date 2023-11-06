0cbfa8638908602423f69896ea5389e8d001b2ab
Anastasiya <00012252wiut@gmail.com>
2023-11-06 17:24:14 +0500

    Added DbContext

---
+++ DSCC.API/DSCC.API/MoviesDbContext/MovieContext.cs
@@ -0,0 +1,12 @@
+﻿using DSCC.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSCC.API.MoviesDbContext
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { Database.EnsureCreated(); }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
